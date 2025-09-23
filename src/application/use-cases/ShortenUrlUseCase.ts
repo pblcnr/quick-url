d3583319d4629e2e968594a787139a85ebe3b22c
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-09-23 10:44:27 -0300

    feat(application): implement ShortenUrlUseCase with retry logic

--- src/application/use-cases/ShortenUrlUseCase.ts
+++ src/application/use-cases/ShortenUrlUseCase.ts
@@ -1,3 +1,7 @@
+import { IUrlRepository } from "../../domain/repositories/IUrlRepository";
+import { Url } from "../../domain/entities/Url";
+import { UrlCode } from "../../domain/value-objects/UrlCode";
+
 export interface ShortenUrlInput {
     originalUrl: string;
 }
@@ -10,5 +14,45 @@ export interface ShortenUrlOutput {
 }
 
 export class ShortenUrlUseCase {
-    
+    constructor(
+        private urlRepository: IUrlRepository
+    ) {}
+
+    async execute(input: ShortenUrlInput): Promise<ShortenUrlOutput> {
+        if (!input.originalUrl || input.originalUrl.trim() === '') {
+            throw new Error("URL original é obrigatória");
+        }
+
+        let attempts: number = 0;
+        const maxAttempts: number = 10;
+        let success: boolean = false;
+
+        while (attempts < maxAttempts && !success) {
+            try {
+                attempts++;
+                let urlCode: UrlCode = UrlCode.generate();
+                let url: Url = new Url({
+                    originalUrl: input.originalUrl,
+                    shortCode: urlCode.value,
+                    isActive: true
+                })
+
+                await this.urlRepository.save(url);
+                success = true;
+
+                return {
+                    id: url.id,
+                    shortCode: url.shortCode,
+                    originalUrl: url.originalUrl,
+                    shortUrl: `${process.env.BASE_URL}/${url.shortCode}`
+                };
+            } catch (errors) {
+                if (attempts >= maxAttempts) {
+                    throw new Error("Número de tentativas esgotadas.");
+                }
+            }
+        }
+
+        throw new Error("Falha ao gerar URL após múltiplas tentativas.");
+    }
 }
