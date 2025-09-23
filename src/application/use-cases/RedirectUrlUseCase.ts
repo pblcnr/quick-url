be15d5580c4e87e2da5948b0866111d03849f6a9
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-09-23 15:16:42 -0300

    feat(application): implement RedirectUrlUseCase for ultra-low latency

--- src/application/use-cases/RedirectUrlUseCase.ts
+++ src/application/use-cases/RedirectUrlUseCase.ts
@@ -1,3 +1,6 @@
+import { IUrlRepository } from "../../domain/repositories/IUrlRepository";
+import { Url } from "../../domain/entities/Url";
+
 export interface RedirectUrlInput {
     shortCode: string;
 }
@@ -7,5 +10,23 @@ export interface RedirectUrlOutput {
 }
 
 export class RedirectUrlUseCase {
+    constructor(
+        private urlRepository: IUrlRepository
+    ) {}
 
+    async execute(input: RedirectUrlInput): Promise<RedirectUrlOutput> {
+        let url: Url | null = await this.urlRepository.findByShortCode(input.shortCode);
+
+        if (url === null) {
+            throw new Error("URL não pode ser nula.");
+        }
+
+        if (!url.canRedirect()) {
+            throw new Error("URL não pode ser redirecionada.");
+        }
+
+        return {
+            originalUrl: url.originalUrl
+        };
+    }
 }
