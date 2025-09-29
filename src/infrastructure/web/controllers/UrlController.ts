7defdff4230886ddf07c2821e51d595ea24c51d6
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-09-29 14:59:01 -0300

    feat(web): implement UrlController with shorten and redirect methods

---
+++ src/infrastructure/web/controllers/UrlController.ts
@@ -0,0 +1,40 @@
+import { Request, Response} from 'express';
+import { ShortenUrlUseCase } from '../../../application/use-cases/ShortenUrlUseCase.js';
+import { RedirectUrlUseCase } from '../../../application/use-cases/RedirectUrlUseCase.js';
+
+export class UrlController {
+    constructor(
+        private shortenUrlUseCase: ShortenUrlUseCase,
+        private redirectUrlUseCase: RedirectUrlUseCase
+    ) {}
+
+    async shorten(req: Request, res: Response): Promise<void> {
+        try {
+            const { originalUrl } = req.body;
+
+            if (!originalUrl) {
+                res.status(400).json({ error: "originalUrl é obrigatória"});
+                return;
+            }
+
+            const result = await this.shortenUrlUseCase.execute({ originalUrl });
+
+            res.status(201).json(result);
+
+        } catch (error) {
+            res.status(500).json({ error: "Erro ao encurtar URL"});
+        }
+    }
+
+    async redirect(req: Request, res: Response): Promise<void> {
+        try {
+            const { shortCode } = req.params;
+
+            const result = await this.redirectUrlUseCase.execute({ shortCode });
+
+            res.redirect(result.originalUrl)
+        } catch (error) {
+            res.status(404).json({ error: "Erro ao redirecionar URL"});
+        }
+    }
+}
